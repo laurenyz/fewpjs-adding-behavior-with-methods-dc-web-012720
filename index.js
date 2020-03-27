464d33cd415bb7d6e4a203bcc5396eedcee08e04
Lauren Yu <laurenkyu@gmail.com>
2020-03-27 15:58:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Cat {
     this.sex = sex
   }
   speak() {
-    returns "'[name]' says meow"
+    return `${this.name} says meow!`
   }
 }
 
@@ -14,7 +14,7 @@ class Dog {
     this.sex = sex
   }
   speak() {
-    console.log(`${this.name} says woof!`)
+    return `${this.name} says woof!`
   }
 }
 
@@ -25,13 +25,10 @@ class Bird {
   }
   speak() {
     if (this.sex == "male") {
-      console.log(`It's me!  ${this.name}, the parrot!`)
+      return `It's me!  ${this.name}, the parrot!`
     } else {
-      console.log(`${this.name} says squawk!`)
+      return `${this.name} says squawk!`
     }
   }
 }
 
-cat = new Cat("Sasha", "female")
-console.log(cat.name)
-cat.speak()
